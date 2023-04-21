669d566638a92d036150133a97d82e33e22a9afd
Maksim Akimov <maksimakimov.96@mail.ru>
2023-04-21 19:17:46 +0300

    Add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все
+//  натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int N = ReadInt();
+
+int ReadInt()
+{
+    System.Console.WriteLine("Введите число N: ");
+    return int.Parse(Console.ReadLine());
+}
+
+void DerivationOfNaturalNum(int N)
+{
+    if (N>0)
+    {
+    System.Console.WriteLine(N);
+    DerivationOfNaturalNum(N-1);
+    }
+}
+DerivationOfNaturalNum(N);
